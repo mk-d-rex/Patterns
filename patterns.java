b161346e16bcb195d27094929f24da056b11f2a6
Murali Krishna Nallandhighal <muralikrishna150307@gmail.com>
2026-07-16 22:57:28 +0530

    all 42 patterns

--- patterns.java
+++ patterns.java
@@ -281,6 +281,242 @@ public class patterns {
 	    }
     }
 
+    public static void pattern22(int n){
+        for (int row = 1; row <= n; row++){
+            for (int sp = 1; sp <= n - row; sp++){
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= 2 * row; col++){
+                System.out.print((col <= row)? '*' : '#');
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern23(int n){
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= n; col++){
+                if (row == col || row == 1 || row == n || col == 1 || col == n){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern24(int n){
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= row; col++){
+                System.out.print(row);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern25(int n){
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= row; col++){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern26(int n){
+        int a = 1;
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= row; col++){
+                System.out.print(a++);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern27(int n){
+        for (int row = 1; row <= n; row++){
+            for (int sp = 1; sp <= n - row; sp++){
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++){
+                System.out.print(row);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern28(int n){
+        for (int row = 1; row <= n; row++){
+            for (int sp = 1; sp < row; sp++){
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= n - row + 1; col++){
+                System.out.print(row);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern29(int n){
+        int a = 1;
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= n; col++){
+                System.out.print(a++);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern30(int n){
+        for (int row = 1; row <= n; row++){
+            int start = 1;
+            for (int col = 1; col <= row; col++){
+                System.out.print(start);
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern31(int n){
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= row; col++){
+                System.out.print((row == col)? 1: 0);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern32(int n){
+        char ch = 'A';
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= n; col++){
+                System.out.print(ch++);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern33(int n){
+        char ch = 'A';
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= n; col++){
+                System.out.print(ch);
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+
+    public static void pattern34(int n){
+        for (int row = 1; row <= n; row++){
+            char ch = 'A';
+            for (int col = 1; col <= n; col++){
+                System.out.print(ch++);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern35(int n){
+        char ch = 'A';
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= row; col++){
+                System.out.print(ch);
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+
+    public static void pattern36(int n){
+        char ch = 'A';
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= row; col++){
+                System.out.print(ch++);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern37(int n){
+        for (int row = 1; row <= n; row++){
+            char ch = 'A';
+            for (int col = 1; col <= row; col++){
+                System.out.print(ch++);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern38(int n){
+        char ch = 'A';
+        for (int row = 1; row <= n; row++){
+            for (int sp = 1; sp <= n - row; sp++){
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++){
+                System.out.print(ch + " ");
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+
+    public static void pattern39(int n){
+        char ch = 'A';
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= n; col++){
+                if (col <= row){
+                    System.out.print(col);
+                }
+                else{
+                    System.out.print(ch);
+                }
+            }
+            ch++;
+            System.out.println();
+        }
+    }
+
+    public static void pattern40(int n){
+        char ch = 'A';
+        for (int row = 1; row <= n; row++){
+            for (int chs = 1; chs < row; chs++){
+                System.out.print(ch++);
+            }
+            for (int col = 1; col <= n - row + 1; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern41(int n){
+        for (int row = 1; row <= n; row++){
+            int start = ((row & 1) == 1)? 1: 0;
+            for (int col = 1; col <= row; col++){
+                System.out.print(start);
+                start = 1 - start;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern42(int n){
+        for (int row = 1; row <= n; row++){
+            for (int sp = 1; sp <= n - row; sp++){
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= 2 * row - 1; col++){
+                System.out.print(row);
+            }
+            System.out.println();
+        }
+    }
+
     public static void patternPrinter(int patternNo, int n) {
         switch (patternNo) {
             case 1 -> pattern1(n);
@@ -304,6 +540,27 @@ public class patterns {
             case 19 -> pattern19(n);
 	        case 20 -> pattern20(n);
 	        case 21 -> pattern21(n);
+            case 22 -> pattern22(n);
+            case 23 -> pattern23(n);
+            case 24 -> pattern24(n);
+            case 25 -> pattern25(n);
+            case 26 -> pattern26(n);
+            case 27 -> pattern27(n);
+            case 28 -> pattern28(n);
+            case 29 -> pattern29(n);
+            case 30 -> pattern30(n);
+            case 31 -> pattern31(n);
+            case 32 -> pattern32(n);
+            case 33 -> pattern33(n);
+            case 34 -> pattern34(n);
+            case 35 -> pattern35(n);
+            case 36 -> pattern36(n);
+            case 37 -> pattern37(n);
+            case 38 -> pattern38(n);
+            case 39 -> pattern39(n);
+            case 40 -> pattern40(n);
+            case 41 -> pattern41(n);
+            case 42 -> pattern42(n);
         }
     
     }
